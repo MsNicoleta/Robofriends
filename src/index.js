8c71c4d2bccc15aa9597ac9da810007bc25abbc2
NG <msnicoleta@outlook.com>
2023-04-27 12:44:44 +0100

    -a

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import App from './containers/App'
+import App from './App'
 import 'tachyons';
-import './containers/App.css';
+import './App.css';
 
 
 
